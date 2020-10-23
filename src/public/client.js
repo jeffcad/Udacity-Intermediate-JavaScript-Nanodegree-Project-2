6b6b6c6bab964e9b2f916bcff52be52325a18939
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-10-23 17:29:47 +0900

    code: Remove apod (astronomy photo starter code)

--- src/public/client.js
+++ src/public/client.js
@@ -1,6 +1,5 @@
 let store = {
     user: { name: "Student" },
-    apod: '',
     selectedRover: '',
     data: '',
     rovers: ['Spirit', 'Opportunity', 'Curiosity'],
@@ -22,7 +21,6 @@ const render = async (root, state) => {
 
 // create content
 const App = (state) => {
-    // let { apod } = state
 
     return `
         <header><h1>Mars Rover Dashboard</h1></header>
@@ -43,34 +41,6 @@ window.addEventListener('load', () => {
 })
 
 // ------------------------------------------------------  COMPONENTS
-
-// Example of a pure function that renders infomation requested from the backend
-// const ImageOfTheDay = (apod) => {
-
-// If image does not already exist, or it is not from today -- request it again
-// const today = new Date()
-// const photodate = new Date(apod.date)
-// console.log(photodate.getDate(), today.getDate());
-
-// console.log(photodate.getDate() === today.getDate());
-// if (!apod || apod.date === today.getDate()) {
-//     getImageOfTheDay(store)
-// }
-
-// check if the photo of the day is actually type video!
-//     if (apod.media_type === "video") {
-//         return (`
-//             <p>See today's featured video <a href="${apod.url}">here</a></p>
-//             <p>${apod.title}</p>
-//             <p>${apod.explanation}</p>
-//         `)
-//     } else {
-//         return (`
-//             <img src="${apod.image.url}" height="350px" width="100%" />
-//             <p>${apod.image.explanation}</p>
-//         `)
-//     }
-// }
 
 const RoverData = (state) => {
 
